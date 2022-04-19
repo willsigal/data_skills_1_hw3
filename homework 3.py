3024b865056f0b5bdba2f00f40aaa3e2fe601925
Jeff Levy <levyjeff@uchicago.edu>
2022-04-18 22:44:04 -0500

    Update homework 3.py

--- homework 3.py
+++ homework 3.py
@@ -7,7 +7,7 @@
 # YOUR CANVAS NAME HERE
 # YOUR GITHUB USER NAME HERE
 
-# Due date: Sunday April 25th before midnight
+# Due date: Sunday April 24th before midnight
 # Write your answers in the space between the questions, and commit/push only
 # this file to your repo. Note that there can be a difference between giving a
 # "minimally" right answer, and a really good answer, so it can pay to put
@@ -23,15 +23,16 @@
 #   b) The what_to_watch method currently raises a ValueError if you use it
 #       before entering any movies. Modify it using try/except so that it tells
 #       the user what they did wrong instead of raising an error.
-#   c) Create a new class called InteractiveMovieDataBase that inherits MovieDataBase
+#   c) Create a new class called InteractiveMovieDataBase that inherits MovieDataBase.
 #   d) Override the add_movie method in your new class so that if it is called
 #       without arguments, it instead asks for user input to add a title/year/
 #       category/stars, but if it is called with arguments it behaves as before
-#   e) Add some appropriate error checking on the user input, so that they
-#       can't enter something that makes no sense (e.g. title=None or year='dog')
+#   e) Add some appropriate error checking to InteractiveMovieDatabase on the user 
+#       input, so that they can't enter something that makes no sense (e.g. title=None
+#       or year='dog')
 #   f) Add a new method to InteractiveMovieDataBase named movie_rankings, which
 #       returns a list of all the titles in the database currently, ordered
-#       highest ranking to lowest
+#       highest ranking (by stars) to lowest
 #
 # NOTE: Your final submission should have only TWO classes: one (modified)
 #       MovieDataBase, and the new InteractiveMovieDataBase
