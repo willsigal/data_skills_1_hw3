8ae72d65d2263c2b1d142acf5f8541c88f113ab8
levyjeff <levyjeff@uchicago.edu>
2024-04-24 10:21:04 -0500

    Update 2024

---
+++ homework3.py
@@ -0,0 +1,88 @@
+# PPHA 30537
+# Spring 2024
+# Homework 3
+
+# YOUR NAME HERE
+
+# YOUR CANVAS NAME HERE
+# YOUR GITHUB USER NAME HERE
+
+# Due date: Sunday May 5th before midnight
+# Write your answers in the space between the questions, and commit/push only
+# this file to your repo. Note that there can be a difference between giving a
+# "minimally" right answer, and a really good answer, so it can pay to put
+# thought into your work.
+
+##################
+
+#NOTE: All of the plots the questions ask for should be saved and committed to
+# your repo under the name "q1_1_plot.png" (for 1.1), "q1_2_plot.png" (for 1.2),
+# etc. using fig.savefig. If a question calls for more than one plot, name them
+# "q1_1a_plot.png", "q1_1b_plot.png",  etc.
+
+# Question 1.1: With the x and y values below, create a plot using only Matplotlib.
+# You should plot y1 as a scatter plot and y2 as a line, using different colors
+# and a legend.  You can name the data simply "y1" and "y2".  Make sure the
+# axis tick labels are legible.  Add a title that reads "HW3 Q1.1".
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import os
+
+x = pd.date_range(start='1990/1/1', end='1991/12/1', freq='MS')
+y1 = np.random.normal(10, 2, len(x))
+y2 = [np.sin(v)+10 for v in range(len(x))]
+
+# Question 1.2: Using only Matplotlib, reproduce the figure in this repo named
+# question_2_figure.png.
+
+# Question 1.3: Load the mpg.csv file that is in this repo, and create a
+# plot that tests the following hypothesis: a car with an engine that has
+# a higher displacement (i.e. is bigger) will get worse gas mileage than
+# one that has a smaller displacement.  Test the same hypothesis for mpg
+# against horsepower and weight.
+
+# Question 1.4: Continuing with the data from question 1.3, create a scatter plot 
+# with mpg on the y-axis and cylinders on the x-axis.  Explain what is wrong 
+# with this plot with a 1-2 line comment.  Now create a box plot using Seaborn
+# that uses cylinders as the groupings on the x-axis, and mpg as the values
+# up the y-axis.
+
+# Question 1.5: Continuing with the data from question 1.3, create a two-by-two 
+# grid of subplots, where each one has mpg on the y-axis and one of 
+# displacement, horsepower, weight, and acceleration on the x-axis.  To clean 
+# up this plot:
+#   - Remove the y-axis tick labels (the values) on the right two subplots - 
+#     the scale of the ticks will already be aligned because the mpg values 
+#     are the same in all axis.  
+#   - Add a title to the figure (not the subplots) that reads "Changes in MPG"
+#   - Add a y-label to the figure (not the subplots) that says "mpg"
+#   - Add an x-label to each subplot for the x values
+# Finally, use the savefig method to save this figure to your repo.  If any
+# labels or values overlap other chart elements, go back and adjust spacing.
+
+# Question 1.6: Are cars from the USA, Japan, or Europe the least fuel
+# efficient, on average?  Answer this with a plot and a one-line comment.
+
+# Question 1.7: Using Seaborn, create a scatter plot of mpg versus displacement,
+# while showing dots as different colors depending on the country of origin.
+# Explain in a one-line comment what this plot says about the results of 
+# question 1.6.
+
+# Question 2: The file unemp.csv contains the monthly seasonally-adjusted unemployment
+# rates for US states from January 2020 to December 2022. Load it as a dataframe, as well
+# as the data from the policy_uncertainty.xlsx file from homework 2 (you do not have to make
+# any of the changes to this data that were part of HW2, unless you need to in order to 
+# answer the following questions).
+#    2.1: Merge both dataframes together
+#    2.2: Calculate the log-first-difference (LFD) of the EPU-C data
+#    2.2: Select five states and create one Matplotlib figure that shows the unemployment rate
+#         and the LFD of EPU-C over time for each state. Save the figure and commit it with 
+#         your code.
+#    2.3: Using statsmodels, regress the unemployment rate on the LFD of EPU-C and fixed
+#         effects for states. Include an intercept.
+#    2.4: Print the summary of the results, and write a 1-3 line comment explaining the basic
+#         interpretation of the results (e.g. coefficient, p-value, r-squared), the way you 
+#         might in an abstract.
